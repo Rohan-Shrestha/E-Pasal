ff881b1ad392e4dd96883992b4b7d068d700a83a
Rohan Shrestha <rohanstha232@gmail.com>
2023-03-07 19:01:44 +0545

    Header Menu (section, categories and subcategories) are made dynamic

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -9,8 +9,12 @@ class Section extends Model
 {
     use HasFactory;
 
-    public function categories()
-    {
+    public static function sections(){
+        $getSections = Section::with('categories')->where('status',1)->get()->toArray();
+        return $getSections;
+    }
+
+    public function categories(){
         return $this->hasMany('App\Models\Category','section_id')->where(['parent_id'=>0,'status'=>1])->with('subcategories');
     }
 }
