5500d13458f6590388f2ba956b04ec7d99e1387c
Rohan Shrestha <rohanstha232@gmail.com>
2023-03-11 20:53:06 +0545

    Newly Added Products are shown in New Arrivals and their discounted price is also shown if any

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use App\Models\Banner;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class IndexController extends Controller
 {
     public function index(){
         $sliderBanners = Banner::where('type','Slider')->where('status',1)->get()->toArray();
         $fixedBanners = Banner::where('type','Fixed')->where('status',1)->get()->toArray();
-        return view('front.index')->with(compact('sliderBanners','fixedBanners'));
+        $newProducts = Product::orderBy('id','Desc')->where('status',1)->limit(6)->get()->toArray();
+        // dd($newProducts);
+        return view('front.index')->with(compact('sliderBanners','fixedBanners','newProducts'));
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -24,4 +24,25 @@ class Product extends Model
     public function images(){
         return $this->hasMany('App\Models\ProductsImage');
     }
+
+    public static function getDiscountPrice($product_id)
+    {
+        # proDetails = product Details and catDetails = category details
+        $proDetails = Product::select('product_price','product_discount','category_id')->where('id',$product_id)->first();
+        $proDetails = json_decode(json_encode($proDetails),true);
+        $catDetails = Category::select('category_discount')->where('id',$proDetails['category_id'])->first();
+        $catDetails = json_decode(json_encode($catDetails),true);
+
+        if($proDetails['product_discount']>0){
+            // If product discount is added from the admin panel, calucating discounted price
+            $discounted_price = $proDetails['product_price'] - ($proDetails['product_price']*$proDetails['product_discount']/100);
+
+        }else if($catDetails['category_discount']>0){
+            // If product discount isn't added instead category discount is added from the admin panel, calculate the discounted price
+            $discounted_price = $proDetails['product_price'] - ($proDetails['product_price']*$catDetails['category_discount']/100);
+        }else{
+            $discounted_price = 0;
+        }
+        return $discounted_price;
+    }
 }
