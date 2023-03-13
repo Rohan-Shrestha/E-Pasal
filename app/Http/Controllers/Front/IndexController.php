8d9d2a5175a0c930520d0568c062cc164327eae6
Rohan Shrestha <rohanstha232@gmail.com>
2023-03-13 13:18:30 +0545

    Discounted Products are now shown in the discounted section

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -14,8 +14,10 @@ class IndexController extends Controller
         $fixedBanners = Banner::where('type','Fixed')->where('status',1)->get()->toArray();
         $newProducts = Product::orderBy('id','Desc')->where('status',1)->limit(8)->get()->toArray();
         $bestSellers = Product::where(['is_bestseller'=>'Yes','status'=>1])->inRandomOrder()->get()->toArray();
+        $discountedProducts = Product::where('product_discount','>',0)->where('status',1)->limit(8)->inRandomOrder()->get()->toArray();
         // dd($bestSellers);
         // dd($newProducts);
-        return view('front.index')->with(compact('sliderBanners','fixedBanners','newProducts','bestSellers'));
+        // dd($discountedProducts);
+        return view('front.index')->with(compact('sliderBanners','fixedBanners','newProducts','bestSellers','discountedProducts'));
     }
 }
