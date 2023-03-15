8ad4225ec86d54af44e890016468b3cce8af6613
Rohan Shrestha <rohanstha232@gmail.com>
2023-03-15 20:02:39 +0545

    Added Products Sorting Functionality

--- app/Http/Controllers/Front/ProductsController.php
+++ app/Http/Controllers/Front/ProductsController.php
@@ -18,7 +18,28 @@ class ProductsController extends Controller
             // Getting Category Details
             $categoryDetails = Category::categoryDetails($url);
             // dd($categoryDetails);
-            $categoryProducts = Product::with('brand')->whereIn('category_id',$categoryDetails['catIds'])->where('status',1)->Paginate(3);
+            $categoryProducts = Product::with('brand')->whereIn('category_id',$categoryDetails['catIds'])->where('status',1);
+            
+            // Checking for Sort
+            if(isset($_GET['sort']) && !empty($_GET['sort'])){
+                if($_GET['sort']=="product_latest"){
+                    $categoryProducts->orderby('products.id','Desc');
+                }
+                else if($_GET['sort']=="price_lowest") {
+                    $categoryProducts->orderby('products.product_price','Asc');
+                }
+                else if($_GET['sort']=="price_highest") {
+                    $categoryProducts->orderby('products.product_price','Desc');
+                }
+                else if($_GET['sort']=="name_a_z") {
+                    $categoryProducts->orderby('products.product_name','Asc');
+                }
+                else if($_GET['sort']=="name_z_a") {
+                    $categoryProducts->orderby('products.product_name','Desc');
+                }
+            }
+
+            $categoryProducts = $categoryProducts->Paginate(30);
             // dd($categoryProducts);
             // echo "Category exists"; die;
             return view('front.products.listing')->with(compact('categoryDetails','categoryProducts'));
