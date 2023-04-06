746dd48a172231d89ab31e78c57ee5f62c2cd455
Rohan Shrestha <rohanstha232@gmail.com>
2023-04-06 20:28:52 +0545

    Added Helper Function to update the art badge

--- app/Http/Controllers/Front/ProductsController.php
+++ app/Http/Controllers/Front/ProductsController.php
@@ -300,8 +300,10 @@ class ProductsController extends Controller
             // Update the quantity
             Cart::where('id',$data['cartid'])->update(['quantity'=>$data['qty']]);
             $getCartItems = Cart::getCartItems();
+            $totalCartItems = totalCartItems();
             return response()->json([
                 'status'=>true,
+                'totalCartItems'=>$totalCartItems,
                 'view'=>(String)View::make('front.products.cart_items')->with(compact('getCartItems'))
             ]);
         }
@@ -314,7 +316,9 @@ class ProductsController extends Controller
             // echo "<pre>"; print_r($data); die;
             Cart::where('id',$data['cartid'])->delete();
             $getCartItems = Cart::getCartItems();
+            $totalCartItems = totalCartItems();
             return response()->json([
+                'totalCartItems'=>$totalCartItems,
                 'view'=>(String)View::make('front.products.cart_items')->with(compact('getCartItems'))
             ]);
         }
