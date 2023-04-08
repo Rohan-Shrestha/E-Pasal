60bf32d3f16d6a0e85790e1f109ed0b3df1d9929
Rohan Shrestha <rohanstha232@gmail.com>
2023-04-08 22:51:15 +0545

    Add Discount Coupon Functionality Completed (Admin Panel)

--- app/Http/Controllers/Admin/CouponsController.php
+++ app/Http/Controllers/Admin/CouponsController.php
@@ -8,6 +8,7 @@ use App\Models\Coupon;
 use App\Models\Section;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
 class CouponsController extends Controller
@@ -59,7 +60,81 @@ class CouponsController extends Controller
 
         if($request->isMethod('post')){
             $data = $request->all();
-            echo "<pre>"; print_r($data); die;
+            // echo "<pre>"; print_r($data); die;
+
+            // Products form validations
+            $rules = [
+                'categories' => 'required',
+                'brands' => 'required',
+                'coupon_option' => 'required',
+                'coupon_type' => 'required',
+                'amount_type' => 'required',
+                'amount' => 'required|numeric',
+                'expiry_date' => 'required',
+            ];
+
+            $customMessages = [
+                'categories.required' => 'Please select the Categories.',
+                'brands.required' => 'Please select the Brands.',
+                'coupon_option.required' => 'Please select a Coupon Option.',
+                'coupon_type.required' => 'Please select a Coupon Type',
+                'amount_type.required' => 'Please select an Amount Type',
+                'amount.required' => 'Please enter the Amount.',
+                'amount.numeric' => 'Amount must be numeric.',
+                'expiry_date.required' => 'Please select an Expiry Date.',
+            ];
+
+            $this->validate($request, $rules, $customMessages);
+
+            if(isset($data['categories']))
+            {
+                $categories = implode(",", $data['categories']);
+            } else {
+                $categories = "";
+            }
+
+            if(isset($data['brands']))
+            {
+                $brands = implode(",", $data['brands']);
+            } else {
+                $brands = "";
+            }
+
+            if(isset($data['users']))
+            {
+                $users = implode(",", $data['users']);
+            } else {
+                $users = "";
+            }
+
+            if($data['coupon_option']=="Automatic")
+            {
+                $coupon_code = str_random(8);
+            } else {
+                $coupon_code = $data['coupon_code'];
+            }
+
+            $adminType = Auth::guard('admin')->user()->type;
+
+            if($adminType=="vendor"){
+                $coupon->vendor_id = Auth::guard('admin')->user()->vendor_id;
+            } else {
+                $coupon->vendor_id = 0;
+            }
+
+            $coupon->coupon_option = $data['coupon_option'];
+            $coupon->coupon_code = $coupon_code;
+            $coupon->categories = $categories;
+            $coupon->brands = $brands;
+            $coupon->users = $users;
+            $coupon->coupon_type = $data['coupon_type'];
+            $coupon->amount_type = $data['amount_type'];
+            $coupon->amount = $data['amount'];
+            $coupon->expiry_date = $data['expiry_date'];
+            $coupon->status = 1;
+            $coupon->save();
+
+            return redirect('admin/coupons')->with('success_message', $message);
         }
 
         // Get Sections with Categories and Sub Categories
