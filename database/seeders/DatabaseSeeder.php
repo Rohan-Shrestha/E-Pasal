26a7f2df69f2b9fd484fa6e4fca07064a836acde
Rohan Shrestha <rohanstha232@gmail.com>
2023-04-20 18:55:15 +0545

    Admin/Vendor can update the Order Status of each item in an Order

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -35,6 +35,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(FiltersValuesTableSeeder::class);
         // $this->call(CouponsTableSeeder::class);
         // $this->call(DeliveryAddressTableSeeder::class);
-        $this->call(OrderStatusTableSeeder::class);
+        // $this->call(OrderStatusTableSeeder::class);
+        $this->call(OrderItemStatusTableSeeder::class);
     }
 }
