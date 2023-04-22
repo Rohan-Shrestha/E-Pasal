2a5c89e1c0454041cb2badf672b75e02485c2e21
Rohan Shrestha <rohanstha232@gmail.com>
2023-04-22 22:40:26 +0545

    Order Invoice can be generated in PDF from Admin Panel

--- routes/web.php
+++ routes/web.php
@@ -152,6 +152,7 @@ Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function
 
         // ORDER INVOICE
         Route::get('orders/invoice/{id}', 'OrderController@viewOrderInvoice');
+        Route::get('orders/invoice/pdf/{id}', 'OrderController@viewPDFInvoice');
     });
     
     
